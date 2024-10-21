bfeabe88a695d0872fcfdfaed5b6b91249eefc09
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-20 20:21:29 -0400

    Handle stock availability and disable purchase button when out of stock

--- cc10.js
+++ cc10.js
@@ -7,11 +7,22 @@ let prices = {
 
 function updatePrice(){
     const turboDropdown = document.getElementById(`turbo-options`);
-    const selectedTurbo = turboDropdown.ariaValueMax;
+    const selectedTurbo = turboDropdown.value;
     const priceDisplay = document.getElementById(`product-price`);
 
     const newPrice = prices[selectedTurbo];
     priceDisplay.textContent = `$${newPrice}`;
+
+    const selectedOption =turboDropdown.options[turboDropdown.selectedindex];
+    const availability = selectedOption.getAttribute(`data-available`);
+
+    const purchaseButton.disabled = document.getElementById('purchase-button');
+    if (availability === 'out-of-stock'){
+        purchaseButton.disabled = true;
+    }
+    else{
+        purchaseButton.disabled = false;
+    }
 }
 
 const turboDropdown = document.getElementById(`turbo-options`);
